dac057137ce5c70ea55392f8ab1667677f0c93c9
Pietro Mori <pietro.mori@outlook.com>
2024-09-15 12:22:37 +0200

    flex layout. aggiunto Grid e SimpleGrid per navbar e main rispettivamente. -> grid contiene navbar (si ridimensione in automatico) e il main -> silmple grid divisa in  ringcard e altri componenti !!!provato flex che contiene simplegrid per cenrtare tutto il main ma non funziona!!!

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -5,6 +5,8 @@ import { RingCard } from '@/components/RingCard/RingCard';
 import { StatsControls } from '@/components/StatsControls/StatsControls';
 import { StatsGrid } from '@/components/StatsGrid/StatsGrid';
 import { LineChart } from '@mantine/charts';
+import { Grid } from '@mantine/core';
+import { SimpleGrid } from '@mantine/core';
 
 const data = [
   { date: 'Jan', temperature: -25 },
@@ -24,50 +26,66 @@ const data = [
 export function HomePage() {
   return (
     <div>
-      <Flex
-        mih={50}
-        justify="flex-start"
-        align="flex-start"
-        direction="row"
-        wrap="nowrap"
-        flex="1"
-      >
-        <Navbar />
-        <Flex
-          mih={50}
-          gap="xl"
-          justify="center"
-          align="center"
-          direction="row"
-          wrap="wrap"
-          flex="2"
-        >
-          <RingCard />
-          <RingCard />
-          <RingCard />
-          <StatsGrid />
-          <StatsControls />
-          <LineChart
-            h={300}
-            data={data}
-            series={[{ name: 'temperature', label: 'Avg. Temperature' }]}
-            dataKey="date"
-            type="gradient"
-            gradientStops={[
-              { offset: 0, color: 'red.6' },
-              { offset: 20, color: 'orange.6' },
-              { offset: 40, color: 'yellow.5' },
-              { offset: 70, color: 'lime.5' },
-              { offset: 80, color: 'cyan.5' },
-              { offset: 100, color: 'blue.5' },
-            ]}
-            strokeWidth={5}
-            curveType="natural"
-            yAxisProps={{ domain: [-25, 40] }}
-            valueFormatter={(value) => `${value}°C`}
-          />
-        </Flex>
-      </Flex>
+      <Grid overflow="hidden">
+        <Grid.Col span="content">
+          <Navbar />
+        </Grid.Col>
+        <Grid.Col span="auto">
+          <Flex
+            justify="center"
+            align="center"
+            style={{ width: '100%' }}
+          >
+            <SimpleGrid cols={1} verticalSpacing="xl">
+              <Flex
+                mih={50}
+                gap="xl"
+                justify="center"
+                align="center"
+                direction="row"
+                wrap="wrap"
+              >
+                <RingCard />
+                <RingCard />
+                <RingCard />
+              </Flex>
+              <Flex
+                mih={50}
+                gap="xl"
+                justify="center"
+                align="center"
+                direction="row"
+                wrap="wrap"
+              >
+                <StatsGrid />
+                <StatsControls />
+                <div style={{ width: '100%', maxWidth: '800px' }}>
+                  <LineChart
+                    h={300}
+                    data={data}
+                    series={[{ name: 'temperature', label: 'Avg. Temperature' }]}
+                    dataKey="date"
+                    type="gradient"
+                    gradientStops={[
+                      { offset: 0, color: 'red.6' },
+                      { offset: 20, color: 'orange.6' },
+                      { offset: 40, color: 'yellow.5' },
+                      { offset: 70, color: 'lime.5' },
+                      { offset: 80, color: 'cyan.5' },
+                      { offset: 100, color: 'blue.5' },
+                    ]}
+                    strokeWidth={5}
+                    curveType="natural"
+                    yAxisProps={{ domain: [-25, 40] }}
+                    xAxisProps={{ padding: { left: 30, right: 30 } }}
+                    valueFormatter={(value) => `${value}°C`}
+                  />
+                </div>
+              </Flex>
+            </SimpleGrid>
+          </Flex>
+        </Grid.Col>
+      </Grid>
     </div>
   );
 }
